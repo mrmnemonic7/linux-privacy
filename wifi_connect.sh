0ff7b4b44304e8bd88687703888a3369042c84a1
Mr Mnemonic <mrmnemonic7@protonmail.com>
2020-04-05 11:00:18 +1000

    Fix variable usage and clean-up output

--- wifi_connect.sh
+++ wifi_connect.sh
@@ -9,20 +9,20 @@ IPROUTE=192.168.1.1
 echo "WiFi Connect"
 
 echo -n "Dropping connection"
-killall dhclient
+killall dhclient &> /dev/null
 killall wpa_supplicant
 ifconfig ${WLAN} down
 ifconfig ${WLAN} up
 echo "Done"
 
 echo -n "Negotiating WPA..."
-wpa_supplicant -B -t -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf -f /var/log/wpa.log
+wpa_supplicant -B -t -i ${WLAN} -c /etc/wpa_supplicant/wpa_supplicant.conf -f /var/log/wpa.log
 sleep 5
 echo "Done"
 
 if [ "${DYNIP}" == 'y' ]; then
 echo -n "Requesting IP address..."
-dhclient wlan0 &> /dev/null
+dhclient ${WLAN} &> /dev/null
 else
 echo -n "Setting manual IP address and default route..."
 ifconfig ${WLAN} ${IPADDR}
