a44d4ea6e0f5b7782851a8c0eee732c69a01c38c
MrMnemonic7 <mrmnemonic7@protonmail.ch>
2023-02-14 13:51:31 +1100

    Tweak yoyo.org retrieval and add someonewhocares.org

--- update_adblock.sh
+++ update_adblock.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo -n "Updating PGL Yoyo Adblock list..."
-wget "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro=0&startdate%5Bday%5D=01&startdate%5Bmonth%5D=01&startdate%5Byear%5D=2000&mimetype=plaintext" -O /etc/dnsmasq.d/adblock.conf
+curl -s "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=dnsmasq&showintro=0&startdate%5Byear%5D=2000&mimetype=plaintext" -o /etc/dnsmasq.d/adblock.conf
+echo "Done"
+
+echo -n "Updating someonewhocares super list..."
+curl -s https://someonewhocares.org/hosts/hosts | grep -v "#" | sed '/^$/d' | sed 's/\ /\\ /g' | grep -v '^\\' | grep -v '\\$' | awk '{print $2}' | grep -v '^\\' | grep -v '\\$' | sort | awk '{print "address=/" $1 "/127.0.0.1"}' > /etc/dnsmasq.d/superlist.conf
 echo "Done"
 
 echo "Please remember to restart dnsmasq for these updates to take effect"
