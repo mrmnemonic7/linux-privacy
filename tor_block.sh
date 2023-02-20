a60b8550b41e9e4e0fe44257d52b31fd73531509
MrMnemonic7 <mrmnemonic7@protonmail.ch>
2023-02-21 00:27:52 +1100

    Try and improve non-Tor traffic blocker

--- tor_block.sh
+++ tor_block.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 echo -n "Blocking all external non-tor traffic"
-iptables -F
-iptables -P OUTPUT DROP
-iptables -A OUTPUT -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT
+iptables -F OUTPUT
 iptables -A OUTPUT -m owner --uid-owner debian-tor -j ACCEPT
-iptables -P INPUT DROP
-iptables -A INPUT -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 
+iptables -A OUTPUT -o 127.0.0.1 -j ACCEPT
+iptables -P OUTPUT DROP
 echo "Done"
+
+iptables -L -v
